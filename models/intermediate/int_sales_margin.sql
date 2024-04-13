e7769f7292beb885d9690cb82a4794a770dd9ed0
Peter Jacobsen <pietro.jacobsen@gmail.com>
2024-04-13 12:16:50 +0000

    hh

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,4 +1,12 @@
-SELECT *
-FROM {{ ref('stg_raw_sales') }} 
-JOIN {{ ref('stg_raw_product') }} 
-USING(products_id)
+SELECT 
+    products_id, 
+    date_date, 
+    orders_id,
+    revenue, 
+    quantity, 
+    CAST(purchase_price AS FLOAT64), 
+    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
+    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+FROM {{ref("stg_raw__sales")}} s
+LEFT JOIN {{ref("stg_raw__product")}} p 
+    USING (products_id)
