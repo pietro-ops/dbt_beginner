09df49ad9cb6052045fdd8c17d82e202af865132
Peter Jacobsen <pietro.jacobsen@gmail.com>
2024-04-13 11:06:47 +0000

    updates

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -1,22 +1,8 @@
-with 
+with
 
-source as (
+    source as (select * from {{ source("raw", "sales") }}),
 
-    select * from {{ source('raw', 'sales') }}
+    renamed as (select date_date, orders_id, pdt_id, revenue, quantity from source)
 
-),
-
-renamed as (
-
-    select
-        date_date,
-        orders_id,
-        pdt_id,
-        revenue,
-        quantity
-
-    from source
-
-)
-
-select * from renamed
+select *
+from renamed
